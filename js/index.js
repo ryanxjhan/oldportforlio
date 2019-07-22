6269ae211de98ac6da2d98b30c4f7e9aa542cf8f
Ryan Han <ryan.han@uwaterloo.ca>
2019-07-22 13:43:00 -0400

    update

--- js/index.js
+++
@@ -1,7 +0,0 @@
-// Add squares
-
-const squares = document.querySelector('.squares');
-for (var i = 1; i < 365; i++) {
-  const level = Math.floor(Math.random() * 3);  
-  squares.insertAdjacentHTML('beforeend', `<li data-level="${level}"></li>`);
-}
